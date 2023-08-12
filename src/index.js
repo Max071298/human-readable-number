6b94f9be61c9c61d98bd430b9efd4782d1c2a091
Max071298 <102859892+Max071298@users.noreply.github.com>
2023-08-12 12:51:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const numbers1to19 = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const numbers20to90 = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    let nmbrToString = number.toString();
+
+    switch (nmbrToString.length) {
+        case 1:
+            nmbrToString = "00" + nmbrToString;
+            break;
+        case 2:
+            nmbrToString = "0" + nmbrToString;
+            break;
+        case 3:
+            nmbrToString;
+            break;
+    }
+
+    let result = "";
+    Number(nmbrToString.slice(-2)) >= 0 && Number(nmbrToString.slice(-2)) <= 19
+        ? (result = `${numbers1to19[Number(nmbrToString[0])]} hundred ${
+              numbers1to19[Number(nmbrToString.slice(-2))]
+          }`)
+        : (result = `${numbers1to19[Number(nmbrToString[0])]} hundred ${
+              numbers20to90[Number(nmbrToString[1]) - 2]
+          } ${numbers1to19[Number(nmbrToString[2])]}`);
+    return result.replace("zero hundred ", "").replace(" zero", "");
+};
